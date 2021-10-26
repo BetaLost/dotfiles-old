52a92fe8475bf37fd3c8f618e6754ce6b1cd4f00
Beta_Lost <42658940+BetaLost@users.noreply.github.com>
2021-10-26 12:05:45 +0300

    Delete awesome directory

--- awesome/themes/powerarrow/s_wall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-file=$(ls -1 $HOME/.config/awesome/themes/powerarrow/wallpapers/ | sort --random-sort | head -1)
-feh --bg-scale $HOME/.config/awesome/themes/powerarrow/wallpapers/$file
-wal -i $HOME/.config/awesome/themes/powerarrow/wallpapers/$file
-cp $HOME/.cache/wal/colors.json $HOME/.config/awesome/themes/powerarrow/colors.json
