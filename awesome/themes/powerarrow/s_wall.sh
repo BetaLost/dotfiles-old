162acc9063a35f761bb57a5035a607f06eeadc76
Beta_Lost <42658940+BetaLost@users.noreply.github.com>
2021-10-26 09:04:21 +0300

    Update s_wall.sh

--- awesome/themes/powerarrow/s_wall.sh
+++ awesome/themes/powerarrow/s_wall.sh
@@ -3,3 +3,4 @@
 file=$(ls -1 $HOME/.config/awesome/themes/powerarrow/wallpapers/ | sort --random-sort | head -1)
 feh --bg-scale $HOME/.config/awesome/themes/powerarrow/wallpapers/$file
 wal -i $HOME/.config/awesome/themes/powerarrow/wallpapers/$file
+cp $HOME/.cache/wal/colors.json $HOME/.config/awesome/themes/powerarrow/colors.json
