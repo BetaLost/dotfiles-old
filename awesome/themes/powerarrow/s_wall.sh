59c87b19fc52daaffb5e7a59f3807a543dd34b99
Beta_Lost <42658940+BetaLost@users.noreply.github.com>
2021-10-27 18:05:44 +0300

    Add files via upload

--- awesome/themes/powerarrow/s_wall.sh
+++ awesome/themes/powerarrow/s_wall.sh
@@ -2,6 +2,7 @@
 
 # Set random wallpaper
 theme_dir="$HOME/.config/awesome/themes/powerarrow"
+rofi_theme="$HOME/.config/rofi/theme.rasi"
 
 rm $theme_dir/theme.lua
 
@@ -15,7 +16,12 @@ cp $theme_dir/theme.lua.base $theme_dir/theme.lua
 
 get_color() {
 	color=$(jq .colors.color$1 $theme_dir/colors.json)
-	sed -i "s/c$1/$color/g" $theme_dir/theme.lua
+	if [[ $2 == "rofi" ]]; then
+	    nq_color=$(echo $color | tr -d '"')
+	    sed -i "s/c$1/$nq_color/g" $HOME/.config/rofi/theme.rasi 
+	else
+	    sed -i "s/c$1/$color/g" $theme_dir/theme.lua
+	fi
 }
 
 get_color 1
@@ -24,5 +30,9 @@ get_color 3
 get_color 4
 get_color 5
 get_color 6
+
+rm $rofi_theme
+cp "$rofi_theme.base" $rofi_theme
+get_color 1 rofi
 
 rm $theme_dir/colors.json
