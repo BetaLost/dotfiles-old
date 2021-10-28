581ecdfde44cb2ca8214295a1b6bef08dea4032c
Beta_Lost <42658940+BetaLost@users.noreply.github.com>
2021-10-28 09:38:07 +0300

    Add files via upload

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -224,9 +224,9 @@ end
 -- {{{ Mouse bindings
 
 root.buttons(mytable.join(
-    awful.button({ }, 3, function () awful.util.mymainmenu:toggle() end),
-    awful.button({ }, 4, awful.tag.viewnext),
-    awful.button({ }, 5, awful.tag.viewprev)
+    awful.button({ }, 3, function () awful.util.mymainmenu:toggle() end)
+    -- awful.button({ }, 4, awful.tag.viewnext),
+    -- awful.button({ }, 5, awful.tag.viewprev)
 ))
 
 -- }}}
